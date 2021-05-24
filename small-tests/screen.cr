e1e358ad93b4242718facec7765504f84b306769
Davor Ocelic <docelic@crystallabs.io>
2021-05-25 01:59:08 +0200

    Minor

---
+++ small-tests/screen.cr
@@ -0,0 +1,14 @@
+require "../src/crysterm"
+
+include Crysterm
+
+a = App.new
+
+s = Widget::Screen.new #app: a
+
+i = Widget::TextArea.new width: 40, height: 20, top: "center", left: "center" #, border: true #, screen: s
+#s.append i
+
+#STDERR.puts i.focused?
+
+a.exec
