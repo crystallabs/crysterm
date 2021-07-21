dffb8a5e60ae0be477ff2e721e8ec725cc4e0571
Davor Ocelic <docelic@crystallabs.io>
2021-07-21 22:55:17 +0200

    crystal tool format

--- examples/chat.cr
+++ examples/chat.cr
@@ -96,10 +96,10 @@ class MyProg
         end
       end
       chat.scroll_to chat.get_content.lines.size
-      #s.render
+      # s.render
       sleep rand 2
       lag.filled = rand 100
-      #s.render
+      # s.render
     end
   end
 
