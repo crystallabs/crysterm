0ece046470ba96355830629f9429a0d6336c9f9d
Davor Ocelic <docelic@crystallabs.io>
2021-08-21 12:29:55 +0200

    Add missing file

---
+++ src/mixin/pos.cr
@@ -0,0 +1,28 @@
+module Crysterm
+  module Mixin
+    module Pos
+      # Number of times object was rendered
+      property renders = 0
+
+      # Absolute left offset.
+      property aleft : Int32? = nil
+
+      # Absolute top offset.
+      property atop : Int32? = nil
+
+      # Absolute right offset.
+      property aright : Int32? = nil
+
+      # Absolute bottom offset.
+      property abottom : Int32? = nil
+
+      # Absolute position on screen
+      property position = Tput::Position.new
+
+      property? scrollable = false
+
+      # Last rendered position
+      property lpos : LPos? = nil
+    end
+  end
+end
