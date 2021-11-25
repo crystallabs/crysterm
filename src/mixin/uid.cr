42341aaa2c679b47e5950d50e8eb2c8671530ec0
Davor Ocelic <docelic@crystallabs.io>
2021-11-25 20:48:31 +0100

    Make widget uid generation atomic

--- src/mixin/uid.cr
+++ src/mixin/uid.cr
@@ -2,15 +2,15 @@ module Crysterm
   module Mixin
     module Uid
       macro included
-        @@uid = Atomic.new 0i32
+        @@uid : Atomic(Int32) = Atomic.new 0i32
       end
 
       # Unique ID. Auto-incremented.
       #
-      # NOTE This is an instance var; setting it to the value of `@@uid` happens elsewhere.
-      property uid : Atomic(Int32)
+      # NOTE This is an instance var; setting it to the value of `@@uid` happens in includers.
+      property uid : Int32
 
-      def next_uid
+      def next_uid : Int32
         @@uid.add 1
       end
     end
