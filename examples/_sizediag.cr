76228e2095012efe44021d3163505f43c61a90c3
Davor Ocelic <docelic@crystallabs.io>
2026-06-26 00:08:54 +0200

    Remove cruft

--- examples/_sizediag.cr
+++
@@ -1,9 +0,0 @@
-require "../src/crysterm"
-include Crysterm
-# Two screens on two different terminals; report each one's detected size.
-ina = File.open(ARGV[0], "r"); outa = File.open(ARGV[0], "w"); outa.sync = true
-inb = File.open(ARGV[1], "r"); outb = File.open(ARGV[1], "w"); outb.sync = true
-sa = Crysterm::Screen.new(title: "A", input: ina, output: outa)
-sb = Crysterm::Screen.new(title: "B", input: inb, output: outb)
-STDERR.puts "screen A (#{ARGV[0]}): #{sa.awidth}x#{sa.aheight}"
-STDERR.puts "screen B (#{ARGV[1]}): #{sb.awidth}x#{sb.aheight}"
